271879a6d72d19327fd2accbb15fe8a63eb72a4d
MichielSnoek <michiel.snoek@gmail.com>
2020-06-17 10:09:15 +0200

    changed env files to work with Heroku deployment

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const passport = require('passport');
 require('./configs/passport');
 
 mongoose
-    .connect('mongodb://localhost/trails-server', {useNewUrlParser: true})
+    .connect(process.env.MONGODB_URI, {useNewUrlParser: true})
     .then(x => {
         console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
     })
