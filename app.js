3ba920e7964b19b194d510677587f3cf56abfd5c
MichielSnoek <michiel.snoek@gmail.com>
2020-06-23 15:58:59 +0200

    test

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.use((req, res, next) =>{
     res.setHeader('Access-Control-Allow-Methods', 'GET,POST') // Browser send
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
     res.setHeader('Access-Control-Allow-Credentials', 'true')
-    res.setHeader('Set-Cookie', 'HttpOnly;Secure;SameSite=Strict');
+    res.setHeader('Set-Cookie', 'SameSite=None');
     return next()
 })
 
